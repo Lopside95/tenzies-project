df5fe37cd7ae9e86b15db481d425102baa2449a3
James Wallington <jameswallington11@gmail.com>
2023-04-25 16:45:40 +0200

    new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+import Die from "./Die";
+import "./App.css";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  const [ten, setTen] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSame = dice.every((die) => die.value === firstValue);
+
+    if (allHeld && allSame) {
+      setTen(true);
+      console.log("you won");
+    }
+  }, [dice]);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie());
+      // with the updated newDice.push, calling allNewDice creates an array of objects
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
+  function rollDice() {
+    if (!ten) {
+      setDice((prevDice) =>
+        prevDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTen(false);
+      setDice(allNewDice);
+    }
+  }
+
+  function holdDice(id) {
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  // this newGame function works with the ternary of the onClick in the button
+  // but the function can also be put into rollDice()
+  // function newGame() {
+  //   ten && setTen(false);
+  //   setDice(allNewDice);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tenzies-app">
+      <Container>
+        {ten && <Confetti />}
+        <Row>
+          <Col>
+            <div className="dice-container">{diceElements}</div>
+            <button className="roll-dice" onClick={rollDice}>
+              {ten ? "Reset" : "Roll"}
+              {/* <button className="roll-dice" onClick={ten ? newGame : rollDice}> */}
+              {/* {ten ? <h3>Reset</h3> : <h3>Roll</h3>} */}
+            </button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
